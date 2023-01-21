371a2e3435217d5d379b47107cb5df9293d34289
nohumanman <nohumanman@gmail.com>
2023-01-21 17:08:14 +0000

    stopped from taking screenshot

--- SkillcheckExpert.py
+++ SkillcheckExpert.py
@@ -4,6 +4,8 @@ import pyautogui # for screenshots
 import time # for waiting
 from keyboard import press
 from PIL import Image
+import cv2
+import numpy as np
 
 class SkillcheckExpert():
     def __init__(self):
@@ -12,28 +14,45 @@ class SkillcheckExpert():
     def assistance_required(self):
         # the expert is required to do a skillcheck
         start = timer()
-        filename = self._get_screenshot()
+        filename = "screenie.png"#self._get_screenshot()
         time_until_skillcheck = self._time_until_skillcheck(filename)
         end = timer()
         # wait for time until skillcheck minus the time we've wasted
-        time.sleep(abs(time_until_skillcheck - (end - start)))
+        wasted_time = end - start
+        time_to_wait = time_until_skillcheck - wasted_time
+        if time_to_wait < 0:
+            time_to_wait = 0
+            print("Rendering took too long! Skillcheck missed!")
+        time.sleep(time_to_wait)
         self._press_space()
+        print("Time wasted: " + str(wasted_time))
 
     def _time_until_skillcheck(self, filename):
         # returns time until enter key should be pressed
         # this is calculated from the screenshot which
         # contains a red bar and white bar :. image processing
-
+        # note the time for the red line to move halfway along the circle is 0.33 seconds
+        im = cv2.imread(filename)
         # crop image to only contain the skillcheck circle
-        im = Image.open(filename)
-        width, height = im.size
-        left = width / 2 - 200 
-        top = height / 2 - 200
-        right = width / 2 + 200
-        bottom = height / 2 + 150
-        im_crop = im.crop((left, top, right, bottom))
-        im_crop.show()
-        return 0
+        im_crop = self._crop_image_center(im)
+        # get position of red pixels (current skillcheck pos)
+        print(im_crop[0, 0])
+        red_pixels = np.argwhere(cv2.inRange(im_crop, (0,0,120), (20, 20, 255)))
+        avg = np.average(red_pixels, axis=0)
+        cv2.circle(im_crop, (int(avg[0]), int(avg[1])), 5, (255, 0, 0), 1)
+        cv2.circle(im_crop, (int(avg[0]), int(avg[1])), 50, (255, 255, 255), 1)
+        cv2.imwrite("image.png", im_crop)
+        # get position of white pixels (aim)
+        return 2
+  
+    def _crop_image_center(self, im, w = 300, h = 300):
+        # crops the image to only contain the skillcheck circle
+        # returns the cropped image
+        center = im.shape
+        x = center[1]/2 - w/2
+        y = center[0]/2 - h/2
+        im_crop = im[int(y):int(y+h), int(x):int(x+w)]
+        return im_crop
 
     def _get_screenshot(self):
         # get screenshot of the screen
