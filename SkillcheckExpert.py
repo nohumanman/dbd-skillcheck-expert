77e21be19e1941fe1f4a62377cb4bc29fcf1c408
nohumanman <nohumanman@gmail.com>
2023-01-21 15:21:38 +0000

    Update SkillcheckExpert.py

--- SkillcheckExpert.py
+++ SkillcheckExpert.py
@@ -3,6 +3,7 @@ from timeit import default_timer as timer
 import pyautogui # for screenshots
 import time # for waiting
 from keyboard import press
+from PIL import Image
 
 class SkillcheckExpert():
     def __init__(self):
@@ -16,12 +17,22 @@ class SkillcheckExpert():
         end = timer()
         # wait for time until skillcheck minus the time we've wasted
         time.sleep(abs(time_until_skillcheck - (end - start)))
-        self._press_enter()
+        self._press_space()
 
     def _time_until_skillcheck(self, filename):
         # returns time until enter key should be pressed
         # this is calculated from the screenshot which
         # contains a red bar and white bar :. image processing
+
+        # crop image to only contain the skillcheck circle
+        im = Image.open(filename)
+        width, height = im.size
+        left = width / 2 - 200 
+        top = height / 2 - 200
+        right = width / 2 + 200
+        bottom = height / 2 + 150
+        im_crop = im.crop((left, top, right, bottom))
+        im_crop.show()
         return 0
 
     def _get_screenshot(self):
